e19c6e71aad2dc9850a2453daec24ca1339c0ec2
Joonas Heinonen <mrdanglars@gmail.com>
2025-07-27 00:01:31 +0300

    Realistic projectile implemented

---
+++ src/effects/projectile_base.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+class_name ProjectileBase
+
+var speed = 450  # Initial speed of the projectile
+var velocity = Vector2()
+var bullet_gravity = 10  # Adjust this value to change the effect of gravity
+
+func _ready():
+	# Set the initial velocity based on the weapon's rotation
+	var weapon = get_parent().get_node("Player").get_node("WeaponHolder")
+	if weapon:
+		velocity = Vector2(speed, 0).rotated(weapon.rotation)
+	print("Original velocity: ", velocity.angle())
+
+func _physics_process(delta):
+	velocity.y += bullet_gravity
+	position += velocity * delta
+	rotation = velocity.angle()
+
+func _on_projectile_body_entered(body):
+	if body.is_in_group("mobs"):
+		body.queue_free()
+	queue_free()
--- src/player/player.gd
+++ src/player/player.gd
@@ -1,13 +1,19 @@
 extends CharacterBody2D
 
+@export var pistol_bullet : PackedScene
+
 @onready var parent = get_parent()
-@onready var weapon = $WeaponSprite
+@onready var weapon = $WeaponHolder
+@onready var weapon_muzzle = $WeaponHolder/Muzzle
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var ammo : int = 10
+var is_player_allowed_weapon : bool = false
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -19,7 +25,8 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# As good practice, you should replace UI actions with custom
+	# gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -39,15 +46,32 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	wield_weapon()
-	rotate_weapon()
 	move_and_slide()
 
-func rotate_weapon():
-	var mousePos = get_global_mouse_position()
-	weapon.look_at(mousePos)
-
+# Wield the weapon.
 func wield_weapon():
 	if parent is Level and parent.is_weapon_allowed:
 		weapon.show()
+		$ShootLabel.text = "Ammo: " + str(ammo)
+		rotate_weapon()
+
+		# Shooting logic.
+		if Input.is_action_just_pressed("mouse_shoot"):
+			shoot()
 	else:
 		weapon.hide()
+
+# Shoot a projectile.
+func shoot():
+	# if ammo > 0:
+	print(weapon.rotation)
+	var b = pistol_bullet.instantiate()
+	b.rotation = weapon.rotation
+	owner.add_child(b)
+	b.transform = weapon_muzzle.global_transform
+	ammo -= 1
+
+# Rotates the weapon according to mouse position.
+func rotate_weapon():
+	var mousePos = get_global_mouse_position()
+	weapon.look_at(mousePos)
